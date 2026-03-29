bcfb20d6d924c54988d4f06c9cd41c85736765c9
SG170 <swati.gouda2024@vitstudent.ac.in>
2026-03-29 21:45:03 +0530

    your message here

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
-import Greeting from "./Greeting";
-function App() {
-  return (
-<div>
-<h1> First React App</h1>
-<Greeting name="A" time="Morning" />
-<Greeting name="Z" time="Night" />
-</div>
-);
+import User from "./User"
+function App(){
+  return(
+    <div>
+      <h1>Price </h1>
+      <User name   = "Swati" price ="250"/>
+    </div>
+
+  );
 }
-export default App;
+export default App
---
+++ my-app/src/User.js
@@ -0,0 +1,9 @@
+function User({name, price}){
+    return(
+        <div>
+            <h2>Name: {name}</h2>
+            <h3>Age: {price}</h3>
+        </div>
+    );
+}
+export default User
