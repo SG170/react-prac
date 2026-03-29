56fabe84d3cb5af0d900f9c4a0883eb065a50e89
SG170 <swati.gouda2024@vitstudent.ac.in>
2026-03-29 19:21:33 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import Greeting from "./Greeting";
+function App() {
+  return (
+<div>
+<h1> First React App</h1>
+<Greeting name="A" time="Morning" />
+<Greeting name="Z" time="Night" />
+</div>
+);
+}
+export default App;
---
+++ my-app/src/Greeting.js
@@ -0,0 +1,4 @@
+function Greeting(props) {
+ return (<h1>Good {props.time}, {props.name} !</h1>);
+}
+export default Greeting;
